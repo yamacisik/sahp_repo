4b8c82fddaa60555c238fe38eee9264d67c7ca8c
yai3 <yamac.isik@duke.edu>
2021-06-22 03:15:27 -0400

    Training bash file added

--- test.bash
+++ test.bash
@@ -4,7 +4,7 @@ for lr in 0.01 0.001 0.0001
 do
 for es in 0.01 0.001
 do
-python  main_func.py  -e 1000 -t mimic --lr $lr  --early_stop_threshold $es --lambda_l2 0.0
+python  main_func.py  -e 1000 -t mimic --lr $lr  --early-stop-threshold $es --lambda-l2 0.0
 done
 done
 
@@ -14,7 +14,7 @@ for lr in 0.001 0.0001
 do
 for es in 0.01 0.001
 do
-python  main_func.py  -e 1000 -t stackOverflow --lr $lr  --early_stop_threshold $es --lambda_l2 0.0
+python  main_func.py  -e 1000 -t stackOverflow --lr $lr  --early-stop-threshold $es --lambda-l2 0.0
 done
 done
 
@@ -23,6 +23,6 @@ for lr in 0.001 0.0001
 do
 for es in 0.01 0.001
 do
-python  main_func.py  -e 1000 -t retweet --lr $lr  --early_stop_threshold $es --lambda_l2 0.0
+python  main_func.py  -e 1000 -t retweet --lr $lr  --early-stop-threshold $es --lambda-l2 0.0
 done
 done
