d3195392f950f178d0609f43730a61c5ca498dbd
yai3 <yamac.isik@duke.edu>
2021-06-22 23:47:57 -0400

    Added Reproducibility tools and saving mechanism v.3.1

--- test.bash
+++ test.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-python  main_func.py  --save_model True
-python  main_func.py  --save_model -t mimic --lambda-l2 0.0  --early-stop-threshold 0.0001 --lr 0.001
-python  main_func.py  --save_model -t stackOverflow --lambda-l2 0.0  --early-stop-threshold 0.01 --lr 0.0001
-python  main_func.py  --save_model -t retweet --lambda-l2 0.0  --early-stop-threshold 0.001 --lr 0.001
+python  main_func.py  --save_model True -t mimic --lambda-l2 0.0  --early-stop-threshold 0.0001 --lr 0.001
+python  main_func.py  --save_model True -t stackOverflow --lambda-l2 0.0  --early-stop-threshold 0.01 --lr 0.0001
+python  mainbash_func.py  --save_model True -t retweet --lambda-l2 0.0  --early-stop-threshold 0.001 --lr 0.001
