8df649960aead45d5ea0251dee9bef18b12f7e76
yai3 <yamac.isik@duke.edu>
2021-06-23 01:09:06 -0400

    Adding final test results

--- test.bash
+++ test.bash
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python  main_func.py  --save_model True -t mimic --lambda-l2 0.0  --early-stop-threshold 0.0001 --lr 0.001
-python  main_func.py  --save_model True -t stackOverflow --lambda-l2 0.0  --early-stop-threshold 0.01 --lr 0.0001
-python  mainbash_func.py  --save_model True -t retweet --lambda-l2 0.0  --early-stop-threshold 0.001 --lr 0.001
+python  main_func.py  --save_model True -t retweet --lambda-l2 0.0  --early-stop-threshold 0.001 --lr 0.001
