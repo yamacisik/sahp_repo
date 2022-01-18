13233fa9502643971ab769cb5048245e46f2238c
Yamac <yamac619@gmail.com>
2022-01-17 23:05:32 -0500

    Get All results

--- test.bash
+++ test.bash
@@ -5,6 +5,8 @@
 #do
 #python  main_func.py  --save_model True -t synthetic -e 500 -st $st
 #done
-python  main_func.py  --save_model True -t mimic -e 500 --lr 0.001 -es  0.001
+python  main_func.py  --save_model True -t mimic -e 500 --lr 0.0005 -es  0.0001
+python  main_func.py  --save_model True -t mimic -e 500 --lr 0.0001 -es  0.0001
+
 
 #python  main_func.py  --save_model True -t retweet -e 250 --lr 0.0001 -es  0.001 -b 32
